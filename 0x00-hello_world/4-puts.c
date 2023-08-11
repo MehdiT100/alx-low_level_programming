946a452ced5772cfb25243d9eeccdf8610944bee
MehdiT100 <tibta.mehdi@gmail.com>
2023-08-10 17:08:36 -0700

    Task 3 and 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,10 @@
-#!/bin/bash
+#include <stdio.h>
+#include <stdlib.h>
+
+int main( int argc, char * argv[] ) {
+
+    /* Notice that final new line character is implicit */
+    puts( "/"Programming is like building a multilingual puzzle" );        
+        
+    return 0;
+}
