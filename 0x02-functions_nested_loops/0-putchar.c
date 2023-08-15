ffec6579effa14dccb18990197e0e9bae87a320e
MehdiT100 <tibta.mehdi@gmail.com>
2023-08-15 16:06:48 -0700

    First Task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Write a program that prints _putchar, followed by a new line.
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar");
+
+	return (0);
+}
